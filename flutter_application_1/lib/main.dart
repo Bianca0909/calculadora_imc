aaac636957acf7dc9e3c8f8547a702af5ff5824c
bianca.medeiros <bianca.medeiros@triersistemas.com.br>
2022-09-28 20:30:25 -0300

    new commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+  const MyWidget({Key? key}) : super(key: key);
 
   @override
   State<MyWidget> createState() => _MyWidgetState();
@@ -22,44 +22,66 @@ class _MyWidgetState extends State<MyWidget> {
     );
   }
 
-  calcular() {}
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  //Utilizar nas validações:
+  GlobalKey<FormState> formController = GlobalKey<FormState>();
+
+  String mensagem = "Informe os seus dados";
+
+  calcular() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text);
+
+      double imc = peso / (altura * altura);
+      String imcText = imc.toStringAsPrecision(4);
+      //toStringAsPrecision(4) = Mostrar 4 casas após a vírgula
+      // mensagem = imcText;
+      if (imc < 18.6) {
+        mensagem = "Abaixo do peso ($imcText)";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        mensagem = "Peso ideal ($imcText)";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        mensagem = "Levemente acima do peso ($imcText)";
+      } else if (imc >= 29.9 && imc < 24.9) {
+        mensagem = "Obsidade Grau I ($imcText)";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        mensagem = "Obsidade Grau II ($imcText)";
+      } else if (imc >= 40) {
+        mensagem = "Obsidade Grau III ($imcText)";
+      }
+    });
+  }
+
+  limparCampos() {
+    pesoController.text = "";
+    alturaController.text = "";
+
+    setState(() {
+      mensagem = "Informe seus dados";
+      formController = GlobalKey<FormState>();
+    });
+  }
 
   paginaPrincipal() {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(children: [
-          const Icon(
-            Icons.person,
-            size: 200,
-            color: Colors.green,
-          ),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "Digite seu peso",
-              border: OutlineInputBorder(),
-            ),
-          ),
-          const SizedBox(height: 5, width: 100),
-          const TextField(
-              decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: "Digite sua altura",
-          )),
-          const SizedBox(height: 25),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Util().criarBotao("Calcular", 30, Colors.white, Colors.green),
-            ],
-          )
-        ]),
-      ),
-      appBar: AppBar(
-        title: const Text('Calculadora IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
+      appBar: Util().appBar(limparCampos()),
+      body: Column(children: [
+        const SizedBox(height: 20),
+        Util().icone(),
+        Util().criarTextField(TextInputType.number, "Peso (kg)", pesoController,
+            "Informe seu peso"),
+        const SizedBox(height: 20),
+        Util().criarTextField(TextInputType.number, "Altura", alturaController,
+            "Informe sua altura"),
+        const SizedBox(height: 20),
+        Util().criarBotao(formController, "Calcular", 20, Colors.white,
+            calcular(), Colors.green),
+        const SizedBox(height: 20),
+        Util().criarTexto(mensagem, 20, Colors.black)
+      ]),
     );
   }
 }
--- flutter_application_1/lib/util.dart
+++ flutter_application_1/lib/util.dart
@@ -1,6 +1,47 @@
 import 'package:flutter/material.dart';
 
 class Util {
+  appBar(funcao) {
+    return AppBar(
+        title: const Text('Calculadora IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: funcao,
+          ),
+        ]);
+  }
+
+  icone() {
+    return const Icon(
+      Icons.person,
+      size: 200,
+      color: Colors.green,
+    );
+  }
+
+  criarTextField(tipoTeclado, textoEtiqueta, controlador, msg) {
+    return TextFormField(
+      keyboardType: tipoTeclado,
+      decoration: InputDecoration(
+          labelText: textoEtiqueta,
+          labelStyle: const TextStyle(
+            color: Colors.green,
+          )),
+      textAlign: TextAlign.center,
+      style: const TextStyle(color: Colors.green, fontSize: 25.0),
+      controller: controlador,
+      validator: ((value) {
+        if (value!.isEmpty) {
+          return msg;
+        }
+        return null;
+      }),
+    );
+  }
+
   criarTexto(conteudo, tamanho, cor) {
     return Text(
       conteudo,
@@ -9,16 +50,15 @@ class Util {
     );
   }
 
-  criarBotao(conteudoTexto, tamanhoTexto, corTexto, corFundoBotao) {
+  criarBotao(
+      validacao, conteudoTexto, tamanhoTexto, corTexto, funcao, corFundoBotao) {
     return TextButton(
         style: TextButton.styleFrom(
             backgroundColor: corFundoBotao,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(5),
             )),
-        onPressed: () {
-          print("OK");
-        },
+        onPressed: funcao,
         child: criarTexto(conteudoTexto, tamanhoTexto, corTexto));
   }
 }
